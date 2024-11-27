50c412045f714a0abb77b636804ca7ecfeaa7885
Lajos Nagy <lajos.j.nagy@gmail.com>
2024-11-27 07:43:53 +0100

    The first version is ready to go :)

--- ihk_spiel.py
+++ ihk_spiel.py
@@ -1,17 +1,17 @@
 from ihk_spiel_def import *
 
 # State
-
 game_state = {
-    "player_life": 3,
     "welcome_massage": "\nWelcome to CTF game! :)\n",
     "game_rules": "You must answer 3 questions correctly to collect the flag, but you have only three lives!",
+    "player_life": 3,
+    "level": 1,
 
     "question_answer" : {
-        "level1": ["Enter the command to display IP addresses: ", "ipconfig", "I"],
-        "level2": ["How can I display output in the console in Python? ", "print()", "H"],
-        "level3": ["Which OS does Peter hate the most? ", "windows", "K"],
-        "level4": ["Enter the letter you received: ", "IHK"], #TODO: késöbb átírni az IHK-t, hogy ne legyen fix        
+        "level1": ["Enter the command to display IP addresses?", "ipconfig", "I"],
+        "level2": ["How can I display output in the console in Python?", "print()", "H"],
+        "level3": ["Which OS does Peter hate the most?", "windows", "K"],
+        "level4": ["Enter the letter you received: ", "ihk", ""],
     },
 }
 
@@ -20,66 +20,18 @@ while True:
     welcome_massage(game_state)
     game_rules(game_state)
     
-    while game_state["player_life"] > 0:
-        print("\nLvl1") #TODO: kell egy szint kiírató függvény ami a szitet is nyilvántartja (STATE)
-        flag1 = input("Enter the command to display IP addresses: ")
+    while game_state["player_life"] > 0 and game_state["level"] < 5:
+        print(f"Level {game_state["level"]}") 
+        question_printer(game_state, game_state["level"])
+
+        if player_input_and_answer_check(game_state["question_answer"][f"level{game_state['level']}"][1]):
+            answer_is_correct(game_state["question_answer"][f"level{game_state['level']}"][2])
+            game_state["level"] = level_counter(game_state["level"])
         
-        if flag1.lower() == "ipconfig":
-            print("Correct! You receive the letter 'I'.")
-            letter1 = 'I'
-            break
         else:
             game_state["player_life"] = minus_life(game_state["player_life"])
             answer_not_correct(game_state["player_life"])
-            if game_state["player_life"] == 0:
-                you_are_dead_massage()
-                break
-
-    while game_state["player_life"] > 0:
-        print("\nLvl2")
-        flag2 = input("How can I display output in the console in Python? ")
-        
-        if flag2 == "print()":
-            print("Correct! You receive the letter 'H'.")
-            letter2 = 'H'
-            break  
-        else:
-            life = minus_life(life)
-            answer_not_correct(life)
-            if life == 0:
-                you_are_dead_massage()
-                break
-
-    while game_state["player_life"] > 0:
-        print("\nLvl3")
-        flag3 = input("Which OS does Peter hate the most? ")
-        
-        if flag3.lower() == "windows":
-            print("Supertoll! You receive your final letter 'K'.")
-            letter3 = 'K'
-            break
-        else:  
-            life = minus_life(life)
-            answer_not_correct(life)
-            if life == 0:
-                you_are_dead_massage()
-                break
-
-
-    while game_state["player_life"] > 0:
-        CTF = input("\nEnter the letter you received: ")
-
-        if CTF.upper() == f"{letter1}{letter2}{letter3}":
-            print("\nWell done, you captured your first flag!")
-            print(f"The final flag is: GFN") 
-            break
-        else:
-            life = minus_life(life)
-            answer_not_correct(life)
-            if life == 0:
-                you_are_dead_massage()
-                break
-
+                            
     you_won(game_state["player_life"])
     game_state["player_life"] = want_to_try_again(game_state["player_life"])
     
--- ihk_spiel_def.py
+++ ihk_spiel_def.py
@@ -1,31 +1,45 @@
 def welcome_massage(game_state): # print the welcome massage
     if not hasattr(welcome_massage, "has_run"): # check if the function has already run
         welcome_massage.has_run = True
-
         massage = game_state["welcome_massage"]
-        
         return print(massage)
 
 
 def game_rules(game_state): # print the game rules
     if not hasattr(game_rules, "has_run"): # check if the function has already run
         game_rules.has_run = True
-
         massage = game_state["game_rules"]
-
         return print(massage)
-
     massage = "You already know the rules of the game I won't tell you again"
     return print(massage)    
 
+def question_printer(game_state, level): # print the question for the player on the actual level
+    massage = game_state["question_answer"][f"level{level}"][0]
+    return print(massage)
+
+def player_input_and_answer_check(answer): # get the user input (answer)
+    player_answer = input("Enter your answer: ").lower()
+    if player_answer == answer:
+        return True
+    else:
+        return False
+        
+
+def answer_is_correct(letter): # answer is correct print massage
+    print(f"Correct! You receive the letter {letter}.")
+
 
 def answer_not_correct(life): # answer is not correct print massage
-    # massage= "TESZT"
     massage = f"Incorrect! Try again. You lost a life.\nYou have {life} lives left." if life > 0 else "You lost a life.\nYou have no more lives left."
     return print(massage)
 
+
+def level_counter(level): # level counter
+    level += 1
+    return level
+
+
 def minus_life(life): # minus 1 life
-    
     life -= 1
     return life
 
@@ -48,4 +62,3 @@ def want_to_try_again(life): # ask if the player want to try again and reset the
     else: # if the player don't want to try again print goodbye and exit
         print("Goodbye!")
         exit()
-
